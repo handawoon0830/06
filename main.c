59934e2571b8c61248b3bcde414d497934dbee03
handawoon0830 <handawoon0830@naver.com>
2023-10-12 17:49:11 +0900

    5

--- main.c
+++ main.c
@@ -2,27 +2,37 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/*
-void square(int a) {
-	a=a*a;
+int factorial(int n){
+	int a,b;
+	a=1;
+	for(b=1; b<=n; b++) {
+		a=a*b;
+	}
+	return a;
 }
 
-int main()
-{
-	int a=2;
-	square(a);
-	printf("a=%i\n",a);
-}
-*/
+int main() {
+//1. n,r을 입력 받음
+	int n,r; 
+	int div1, div2;
+	int result;
+	
+	// n입력 메세지 풀력 
+	printf("input the n:");
+	// n 스캔
+	scanf("%d",&n);
+	//r입력 메세지 출력
+	printf("input the r:");
+	//r 스캔
+	scanf("%d",&r);
+//2. combination 계산
+	//div1, div2정의
+	div1= factorial(n); //n!
+	div2= factorial(n-r) * factorial(r); //(n-r)! * r!
+	//result= div1/div2 
+	result= div1/div2;
+	printf("C(%d,%d)= %d\n",n,r,result);
+	
+	return 0;
 
-int square( int a )
-{
-	return(a*a);
-}
-
-int main()
-{
-	int a =2;
-	a= square(a);
-	printf("a=%i\n",a);
 }
